b799164ac9265c1fa5ca9c1d352ac25ef3c89201
Tamara Wala <tamwala@gmail.com>
2022-05-13 19:49:26 +0200

    6.2 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int a = 5;
+        int b = 2;
+
+        int resultAddition = calculator.addition(a,b);
+        int resultSubtraction = calculator.subtraction(a, b);
+
+        int expectedAddition = a + b;
+        int expectedSubtraction = a - b;
+
+        if (resultAddition == expectedAddition) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+
+        if (resultSubtraction == expectedSubtraction) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+}
