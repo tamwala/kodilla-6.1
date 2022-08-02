f4ee38eb3bc7b999b3d2121e236bcdb7f6409afe
Tamara Wala <tamwala@gmail.com>
2022-08-02 20:38:19 +0200

    11.5 Kodilla- Builder

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sesame")
+                .burgers(2)
+                .sauce("Barbecue")
+                .ingredient("Onion")
+                .ingredient("Lettuce")
+                .ingredient("Cucumber")
+                .build();
+        System.out.println(bigmac);
+
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+
+        //Then
+        assertEquals(3, howManyIngredients);
+    }
+}
