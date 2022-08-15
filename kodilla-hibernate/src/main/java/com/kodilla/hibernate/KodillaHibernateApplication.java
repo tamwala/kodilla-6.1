0430a931b9d891e24a26258ad6f5ec41efe5febb
Tamara Wala <tamwala@gmail.com>
2022-08-15 13:05:35 +0200

    Kodilla 13.1

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+
+}
