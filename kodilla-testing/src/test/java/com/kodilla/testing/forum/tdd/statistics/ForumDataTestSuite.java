50feb6ee15099327cf8f0fb171e46c55fe7f9873
Tamara Wala <tamwala@gmail.com>
2022-10-12 15:49:52 +0200

    Kodilla 6.7

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/statistics/ForumDataTestSuite.java
@@ -0,0 +1,168 @@
+package com.kodilla.testing.forum.tdd.statistics;
+
+import com.kodilla.testing.forum.statistics.ForumData;
+import com.kodilla.testing.forum.statistics.Statistics;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumDataTestSuite {
+    static int testCounter;
+    Statistics statisticsMock = mock(Statistics.class);
+
+    private List<String> generatedUsersList(int usersQuantity) {
+        List<String> genUsers = new ArrayList<String>();
+        for (int n = 1; n <= usersQuantity; n++) {
+            genUsers.add("User" + n);
+        }
+        return genUsers;
+    }
+
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void calculateAdvStatisticsPosts0() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        users.add("Bolek");
+        users.add("Lolek");
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(0);
+        when(statisticsMock.commentsCount()).thenReturn(20);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(forumData.getAvgCommentsOnPosts() == Double.POSITIVE_INFINITY);
+    }
+
+    @Test
+    public void calculateAdvStatisticsPosts1000() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        users.add("Bolek");
+        users.add("Lolek");
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(20);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(forumData.getAvgCommentsOnPosts() == 2);
+    }
+
+    @Test
+    public void calculateAdvStatisticsComments0() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        users.add("Bolek");
+        users.add("Lolek");
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(forumData.getAvgCommentsOnPosts() == forumData.getAvgCommentsOnUser());
+    }
+
+    @Test
+    public void calculateAdvStatisticsCommentsLessThanPosts() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        users.add("Bolek");
+        users.add("Lolek");
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(100);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(statisticsMock.postsCount() > statisticsMock.commentsCount());
+    }
+
+    @Test
+    public void calculateAdvStatisticsPostsLessThanComments() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        users.add("Bolek");
+        users.add("Lolek");
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(statisticsMock.postsCount() < statisticsMock.commentsCount());
+    }
+
+    @Test
+    public void calculateAdvStatisticsUsers0() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = new ArrayList<>();
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(20);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue((Double) forumData.getAvgCommentsOnUser() == Double.POSITIVE_INFINITY);
+    }
+
+    @Test
+    public void calculateAdvStatisticsUsers100() {
+        //Given
+        ForumData forumData = new ForumData(statisticsMock);
+        List<String> users = generatedUsersList(100);
+
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(100);
+        when(statisticsMock.commentsCount()).thenReturn(20);
+
+        //When
+        forumData.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(forumData.getAvgPostsOnUser() == 1);
+    }
+}
